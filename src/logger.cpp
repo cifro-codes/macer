10eec06d6576335585742c7ebac0586aa2f4f64b
Lee *!* Clagett <code@leeclagett.com>
2024-11-18 13:15:38 -0500

    Remove lock for logging (not needed)

--- src/logger.cpp
+++ src/logger.cpp
@@ -28,13 +28,7 @@
 #include "logger.hpp"
 
 #include <cstring>
-#include <mutex>
 #include <string>
-
-namespace
-{
-  std::mutex log_sync;
-}
 
 void print_error(const std::error_code code, const char* file, const int line, const char* additional)
 {
@@ -42,7 +36,10 @@ void print_error(const std::error_code code, const char* file, const int line, c
   if (!file)
     file = "unknown file";
   if (!additional)
+  {
+    additional = "";
     separator = "";
+  }
 
   const char* filename = std::strrchr(file, '/');
   if (filename)
@@ -51,6 +48,5 @@ void print_error(const std::error_code code, const char* file, const int line, c
     filename = file;
 
   const std::string msg = code.message();
-  const std::lock_guard<std::mutex> lock{log_sync};
   fprintf(stderr, "(%s:%i) %s%s%s\n", filename, line, msg.c_str(), separator, additional);
 }
